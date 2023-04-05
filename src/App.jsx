b813e8f1de23ad0c928681a6d979b19304762987
tuhin_4ever <tuhin4ever19@gmail.com>
2023-04-06 05:40:15 +0600

    start from hare

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <div>
+        {/* Header */}
+        <Outlet/>
+        {/* Footer */}
+    </div>
+  );
+};
+
+export default App;
