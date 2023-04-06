b659a50120098926092c41dc8ea26e440bc7c6fc
tuhin_4ever <tuhin4ever19@gmail.com>
2023-04-06 12:11:13 +0600

    header,home and about section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Header from './components/Header';
 
 const App = () => {
   return (
     <div>
         {/* Header */}
+        <Header/>
         <Outlet/>
         {/* Footer */}
     </div>
